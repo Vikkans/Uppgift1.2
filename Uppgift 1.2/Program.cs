17b329eadec1e36696f4a8fae25a3f5332eb6454
Vikkans <Jakobsenviktor6@gmail.com>
2023-09-06 10:38:52 +0200

    Add project files.

---
+++ Uppgift 1.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;  
+
+namespace Uppgift_1_2
+{
+      class program
+      {
+           static void Main(string[] args)
+           {  
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("DETTA ÄR INTE BRA DIN DATOR KOMMER KRASHA FÖR ATT DU ÄR FÖR COOL VIKTOR DAMN");
+            Console.ReadKey();
+            
+           }
+      }
+
+
+
+
+}
+
+
